d2affb48ebdd12646613a09db03f793470199def
TuananhDo <TuananhDo0308@gmail.com>
2025-05-26 01:57:41 +0700

    update

--- Program.cs
+++ Program.cs
@@ -23,15 +23,6 @@ Console.WriteLine("ASPNETCORE_ENVIRONMENT: " + Environment.GetEnvironmentVariabl
 Console.WriteLine("ConnectionStrings__DefaultConnectionString: " + Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnectionString"));
 
 var builder = WebApplication.CreateBuilder(args);
-if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
-{
-    Env.Load("/app/.env");
-}
-
-// Log after loading .env
-Console.WriteLine("After loading .env:");
-Console.WriteLine("ASPNETCORE_ENVIRONMENT: " + Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"));
-Console.WriteLine("ConnectionStrings__DefaultConnectionString: " + Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnectionString"));
 
 builder.Configuration.Sources.Clear();
 builder.Configuration.AddEnvironmentVariables();
